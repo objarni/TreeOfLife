993cfdeed3feabab1a73ba1ae3d63c311c2cd07e
Olof Bjarnason <olof.bjarnason@gmail.com>
2025-01-12 21:57:32 +0100

    Draw axis in red and green

--- src/TimeLinePanel.kt
+++ src/TimeLinePanel.kt
@@ -34,9 +34,12 @@ class TimeLinePanel : JPanel() {
             zoom = 1.0
         )
 
-        val xaxis = projector.projectRectangle(Point(0, 0), Dimension(20, 2))
-        val yaxis = projector.projectRectangle(Point(0, 0), Dimension(2, 20))
+        g2d.color = Color.RED
+        val xaxis = projector.projectRectangle(Point(0, 0), Dimension(20, 1))
         g2d.drawRect(xaxis.x, xaxis.y, xaxis.width, xaxis.height)
+
+        g2d.color = Color.GREEN
+        val yaxis = projector.projectRectangle(Point(0, 0), Dimension(1, 20))
         g2d.drawRect(yaxis.x, yaxis.y, yaxis.width, yaxis.height)
 
         // Optionally fill the rectangle
