9ea0c7828e508a2d2cb5e24dad24116b317d7d47
Olof Bjarnason <olof.bjarnason@gmail.com>
2025-01-12 14:33:51 +0100

    Tidy: dead code

--- src/Main.kt
+++ src/Main.kt
@@ -27,14 +27,3 @@ private fun createAndShowGUI() {
 fun main() {
     EventQueue.invokeLater(::createAndShowGUI)
 }
-
-
-//
-//fun main() {
-//    val name = "Kotlin"
-//    println("Hello, $name!")
-//
-//    for (i in 1..5) {
-//        println("i = $i")
-//    }
-//}
