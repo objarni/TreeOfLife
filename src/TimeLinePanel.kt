57877cf4627c15d4c8eacb32cc107d91fc25501e
Olof Bjarnason <olof.bjarnason@gmail.com>
2025-01-12 22:01:10 +0100

    Tidy: imports

--- src/TimeLinePanel.kt
+++ src/TimeLinePanel.kt
@@ -1,9 +1,6 @@
 package TreeOfLife
 
-import java.awt.Color
-import java.awt.Dimension
-import java.awt.Graphics
-import java.awt.Point
+import java.awt.*
 import javax.swing.JPanel
 
 class TimeLinePanel : JPanel() {
