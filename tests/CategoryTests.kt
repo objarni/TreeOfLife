2d24438009b325996f1198ae84c879f47eacd401
Olof Bjarnason <olof.bjarnason@gmail.com>
2025-01-16 08:02:25 +0100

    Add empty category test

---
+++ tests/CategoryTests.kt
@@ -0,0 +1,19 @@
+import TreeOfLife.Box
+import TreeOfLife.Month
+import TreeOfLife.Period
+import TreeOfLife.TimePoint
+import TreeOfLife.Year
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import java.awt.Color
+import java.awt.Rectangle
+
+/*
+given a bunch of periods belonging to a category, convert them to boxes that do not overlap
+non overlapping 2 boxes gives output y = 0, 0
+overlapping 2 boxes gives output y = 0, 1
+3 boxes where the 2nd two overlap gives y = 0, 0, 1
+ */
+
+class CategoryTests {
+}
