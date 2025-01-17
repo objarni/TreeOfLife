e91a9b238847a9e8703512bfa08a828fa04a2df2
Olof Bjarnason <olof.bjarnason@gmail.com>
2025-01-17 22:47:10 +0100

    Add all Month constants

--- src/Period.kt
+++ src/Period.kt
@@ -18,7 +18,15 @@ data class Month(val value: Int) {
         val JANUARY = Month(1)
         val FEBRUARY = Month(2)
         val MARCH = Month(3)
+        val APRIL = Month(4)
         val MAY = Month(5)
+        val JUNE = Month(6)
+        val JULY = Month(7)
+        val AUGUST = Month(8)
+        val SEPTEMBER = Month(9)
+        val OCTOBER = Month(10)
+        val NOVEMBER = Month(11)
+        val DECEMBER = Month(12)
     }
 }
 
