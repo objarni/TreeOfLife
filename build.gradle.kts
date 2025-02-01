3d66b09325542421838de8f4fa7e0fe460f5bb90
Olof Bjarnason <olof.bjarnason@gmail.com>
2025-02-01 18:24:03 +0100

    Add gradle build support

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    kotlin("jvm") version "1.8.0"
+    application
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.8.0")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.8.0")
+    testImplementation(kotlin("test"))
+}
+
+application {
+    mainClass.set("TreeOfLife.MainKt")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+
+sourceSets {
+    main {
+        kotlin.srcDirs("src")
+    }
+    test {
+        kotlin.srcDirs("tests")
+    }
+}
