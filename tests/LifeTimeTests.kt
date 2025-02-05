34e6cb2f417b71ce962b7454036903134934d2a2
Olof Bjarnason <olof.bjarnason@gmail.com>
2025-02-05 22:23:54 +0100

    Add chronological test for TimePoints

---
+++ tests/LifeTimeTests.kt
@@ -0,0 +1,30 @@
+class LifeTimeTests {
+//    @Test
+//    fun test10YearsOldPeriods() {
+//        val expected = listOf(
+//            "Rödebo förskola",
+//            "Röstånga"
+//        )
+//        val lifeTime = listOf(
+//            homes(),
+//            educations()
+//        )
+//        val actual = overlappingPeriods(
+//            TimePoint(Year(1989), Month.OCTOBER),
+//            lifeTime
+//        )
+//        assertEquals(expected, actual)
+//    }
+//
+//    private fun overlappingPeriods(
+//        point: TimePoint,
+//        categories: List<Category>
+//    ): List<String> {
+//        return categories.flatMap { category ->
+//            category.periods.filter { period ->
+//                period.start <= point && point <= period.end
+//            }.map { it.name }
+//        }
+//    }
+}
+
---
+++ tests/TimePointTests.kt
@@ -0,0 +1,34 @@
+import TreeOfLife.Data.Month
+import TreeOfLife.Data.TimePoint
+import TreeOfLife.Data.Year
+import kotlin.test.Test
+import kotlin.test.assertFalse
+
+class TimePointTests {
+    @Test
+    fun testChronologicalDifferentYearsOrdered() {
+        val seventies = TimePoint(Year(1979), Month.JULY)
+        val eighties = TimePoint(Year(1989), Month.JUNE)
+        assert(chronological(seventies, eighties))
+    }
+
+    @Test
+    fun testChronologicalDifferentYearsUnordered() {
+        val year2k = TimePoint(Year(2000), Month.JULY)
+        val eigtyNine = TimePoint(Year(1989), Month.JUNE)
+        assertFalse(chronological(year2k, eigtyNine))
+    }
+
+    @Test
+    fun testChronologicalSameYear() {
+        val june = TimePoint(Year(1979), Month.JUNE)
+        val july = TimePoint(Year(1979), Month.JULY)
+        assert(chronological(june, july))
+    }
+
+    private fun chronological(before: TimePoint, after: TimePoint): Boolean {
+        val sameYear = before.year.value == after.year.value
+        return before.year.value < after.year.value
+                || (sameYear && before.month.value < after.month.value)
+    }
+}
