a7c088b3b63c2b6939864beca07bd321ae7b4057
Olof Bjarnason <olof.bjarnason@gmail.com>
2025-02-05 22:00:33 +0100

    Use category functions

--- src/Visualization/VisualCategory.kt
+++ src/Visualization/VisualCategory.kt
@@ -1,15 +1,19 @@
 package TreeOfLife.Visualization
 
 import TreeOfLife.Data.Category
+import TreeOfLife.Data.educations
+import TreeOfLife.Data.employments
+import TreeOfLife.Data.homes
+import TreeOfLife.Data.loveRelationsShips
 import java.awt.Color
 
 data class VisualCategory(val category: Category, val color: Color, val baseY: Int)
 
 fun visualCategories(): List<VisualCategory> {
     return listOf(
-        VisualCategory(Category("Home", TreeOfLife.Data.homePeriods()), Color.ORANGE, 2),
-        VisualCategory(Category("Educations", TreeOfLife.Data.educationPeriods()), Color.BLUE, 4),
-        VisualCategory(Category("Employments", TreeOfLife.Data.employmentPeriods()), Color.DARK_GRAY, 6),
-        VisualCategory(Category("Love relationships", TreeOfLife.Data.loveRelationsShipPeriods()), Color.PINK, 8)
+        VisualCategory(homes(), Color.ORANGE, 2),
+        VisualCategory(educations(), Color.BLUE, 4),
+        VisualCategory(employments(), Color.DARK_GRAY, 6),
+        VisualCategory(loveRelationsShips(), Color.PINK, 8)
     )
 }
