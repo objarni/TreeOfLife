6a2470d22e1998c8539d2d8337ca658c3c485eaa
Olof Bjarnason <olof.bjarnason@gmail.com>
2025-03-29 17:57:56 +0100

    Add tree of life file format parser

--- src/Data/Parsers.kt
+++ src/Data/Parsers.kt
@@ -1,26 +1,42 @@
 package TreeOfLife.Data
 
 
-fun monthParser(string: String): Month? {
-    val month = string.lowercase()
-    if (month.startsWith("jan")) return Month.JANUARY
-    if (month.startsWith("feb")) return Month.FEBRUARY
-    if (month.startsWith("mar")) return Month.MARCH
-    if (month.startsWith("apr")) return Month.APRIL
-    if (month.startsWith("may")) return Month.MAY
-    if (month.startsWith("jun")) return Month.JUNE
-    if (month.startsWith("jul")) return Month.JULY
-    if (month.startsWith("aug")) return Month.AUGUST
-    if (month.startsWith("sep")) return Month.SEPTEMBER
-    if (month.startsWith("oct")) return Month.OCTOBER
-    if (month.startsWith("nov")) return Month.NOVEMBER
-    if (month.startsWith("dec")) return Month.DECEMBER
-    return null
+fun topLevelParser(string: String): Pair<TimePoint, List<Category>> {
+    val lines = string.split("\n")
+    val prefix = "Month of birth:"
+    val birthTimePointLine = lines.firstOrNull { it.startsWith(prefix) }
+    val birthTimePoint = if (birthTimePointLine != null) {
+        timePointParser(birthTimePointLine.removePrefix(prefix).trim())
+    } else {
+        null
+    }
+    val categoriesString = lines.dropWhile { !it.startsWith("---") }.joinToString("\n")
+    val categories = categoriesParser(categoriesString)
+    return Pair(birthTimePoint!!, categories)
+}
+
+fun categoriesParser(string: String): List<Category> {
+    val categories = mutableListOf<Category>()
+    val categoryStrings = string.split("###").map { it.trim() }
+    for (categoryString in categoryStrings) {
+        if (categoryString.isNotBlank()) {
+            val category = categoryParser(categoryString)
+            categories.add(category)
+        }
+    }
+    return categories
+}
+
+fun categoryParser(string: String): Category {
+    val lines = string.split("\n")
+    val categoryName = lines[0].trim().removePrefix("---").removeSuffix("---").trim()
+    val periods = lines.drop(1).mapNotNull { periodParser(it) }
+    return Category(categoryName, periods)
 }
 
 fun periodParser(string: String): Period? {
     val periodShort = periodParserShortFormat(string)
-    if(periodShort != null)
+    if (periodShort != null)
         return periodShort
     val regex = """\s*(.+):\s*([A-Z][a-z]{2})\s*(\d{4})\s*-\s*([A-Z][a-z]{2})\s*(\d{4})""".toRegex()
     val matchResult = regex.find(string)
@@ -61,26 +77,6 @@ fun periodParserShortFormat(string: String): Period? {
     return null
 }
 
-fun categoryParser(string: String) : Category {
-    val lines = string.split("\n")
-    val categoryName = lines[0].trim().removePrefix("---").removeSuffix("---").trim()
-    val periods = lines.drop(1).mapNotNull { periodParser(it) }
-    return Category(categoryName, periods)
-}
-
-fun categoriesParser(string: String): List<Category> {
-    val categories = mutableListOf<Category>()
-    val categoryStrings = string.split("###").map { it.trim() }
-    for (categoryString in categoryStrings) {
-        if (categoryString.isNotBlank()) {
-            val category = categoryParser(categoryString)
-            categories.add(category)
-        }
-    }
-    return categories
-}
-
-
 fun timePointParser(string: String): TimePoint? {
     val regex = """\s*([A-Z][a-z]{2})\s*(\d{4})""".toRegex()
     val matchResult = regex.find(string)
@@ -95,16 +91,19 @@ fun timePointParser(string: String): TimePoint? {
 }
 
 
-fun topLevelParser(string: String): Pair<TimePoint, List<Category>> {
-    val lines = string.split("\n")
-    val prefix = "Month of birth:"
-    val birthTimePointLine = lines.firstOrNull { it.startsWith(prefix) }
-    val birthTimePoint = if (birthTimePointLine != null) {
-        timePointParser(birthTimePointLine.removePrefix(prefix).trim())
-    } else {
-        null
-    }
-    val categoriesString = lines.dropWhile { !it.startsWith("---") }.joinToString("\n")
-    val categories = categoriesParser(categoriesString)
-    return Pair(birthTimePoint!!, categories)
+fun monthParser(string: String): Month? {
+    val month = string.lowercase()
+    if (month.startsWith("jan")) return Month.JANUARY
+    if (month.startsWith("feb")) return Month.FEBRUARY
+    if (month.startsWith("mar")) return Month.MARCH
+    if (month.startsWith("apr")) return Month.APRIL
+    if (month.startsWith("may")) return Month.MAY
+    if (month.startsWith("jun")) return Month.JUNE
+    if (month.startsWith("jul")) return Month.JULY
+    if (month.startsWith("aug")) return Month.AUGUST
+    if (month.startsWith("sep")) return Month.SEPTEMBER
+    if (month.startsWith("oct")) return Month.OCTOBER
+    if (month.startsWith("nov")) return Month.NOVEMBER
+    if (month.startsWith("dec")) return Month.DECEMBER
+    return null
 }
