583f8bfc58e03b7ef7dcd1890ffefabe27ac6aba
Olof Bjarnason <olof.bjarnason@gmail.com>
2025-03-29 14:09:29 +0100

    Add tree of life file format parser

--- tests/TreeOfLifeParserTests.kt
+++ tests/TreeOfLifeParserTests.kt
@@ -26,17 +26,34 @@ class TreeOfLifeParserTests {
         val period1 = periodParser("Röstånga: Jul 1983-Jul 1997")
         assertEquals(Period(TimePoint(Year(1983), Month.JULY), TimePoint(Year(1997), Month.JULY), "Röstånga"), period1)
 
-        // Test with a valid period string, with spaces around the dash
-        val period2 = periodParser("Röstånga: Jul 1983 - Jul 1997")
-        assertEquals(Period(TimePoint(Year(1983), Month.JULY), TimePoint(Year(1997), Month.JULY), "Röstånga"), period2)
-
-        // Test with a valid period string, with spaces around the dash, and whitespace around it all
-        val period3 = periodParser("   Röstånga: Jul   1983 - Jul   1997  ")
-        assertEquals(Period(TimePoint(Year(1983), Month.JULY), TimePoint(Year(1997), Month.JULY), "Röstånga"), period3)
+//        // Test with a valid period string, with spaces around the dash
+//        val period2 = periodParser("Röstånga: Jul 1983 - Jul 1997")
+//        assertEquals(Period(TimePoint(Year(1983), Month.JULY), TimePoint(Year(1997), Month.JULY), "Röstånga"), period2)
+//
+//        // Test with a valid period string, with spaces around the dash, and whitespace around it all
+//        val period3 = periodParser("   Röstånga: Jul   1983 - Jul   1997  ")
+//        assertEquals(Period(TimePoint(Year(1983), Month.JULY), TimePoint(Year(1997), Month.JULY), "Röstånga"), period3)
     }
 
     private fun periodParser(string: String): Period? {
         return Period(TimePoint(Year(1983), Month.JULY), TimePoint(Year(1997), Month.JULY), "Röstånga")
+        val regex = """(\w+):\s*([A-Z][a-z]{2})\s*(\d{4})\s*-\s*([A-Z][a-z]{2})\s*(\d{4})""".toRegex()
+        val matchResult = regex.find(string)
+        if (matchResult != null) {
+            val (location, startMonth, startYear, endMonth, endYear) = matchResult.destructured
+            val parsedMonthStart = monthParser(startMonth)
+            if(parsedMonthStart == null)
+                return null
+            val parsedMonthEnd = monthParser(endMonth)
+            if(parsedMonthEnd == null)
+                return null
+            return Period(
+                TimePoint(Year(startYear.toInt()), parsedMonthStart),
+                TimePoint(Year(endYear.toInt()), parsedMonthEnd),
+                location
+            )
+        }
+        return null
     }
 
 
