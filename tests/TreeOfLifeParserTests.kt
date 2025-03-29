9ac2fc22078fcd07417d28b038148fa1e3ac5d55
Olof Bjarnason <olof.bjarnason@gmail.com>
2025-03-29 11:10:27 +0100

    Add tree of life file format parser

--- tests/TreeOfLifeParserTests.kt
+++ tests/TreeOfLifeParserTests.kt
@@ -10,62 +10,7 @@ import java.awt.Color
 
 class TreeOfLifeParserTests {
     @Test
-    fun testNonOverlapping() {
-        val periods = listOf(
-            Period(TimePoint(Year(2020), Month.JANUARY), TimePoint(Year(2020), Month.FEBRUARY), "Test period 1"),
-            Period(TimePoint(Year(2020), Month.FEBRUARY), TimePoint(Year(2020), Month.MARCH), "Test period 2")
-        )
-        val blocks = textBlocksForPeriods(
-            periods, baseY = 0, color = Color.BLUE, birthMonth = TimePoint(
-            Year(1979), Month.JULY)
-        )
-        assertEquals(0, blocks[0].rect.y)
-        assertEquals(0, blocks[1].rect.y)
-    }
-
-    @Test
-    fun testTwoOverlapping() {
-        val periods = listOf(
-            Period(TimePoint(Year(2020), Month.JANUARY), TimePoint(Year(2020), Month.MARCH), "Test period 1"),
-            Period(TimePoint(Year(2020), Month.FEBRUARY), TimePoint(Year(2020), Month.MAY), "Test period 2")
-        )
-        val blocks = textBlocksForPeriods(
-            periods, baseY = 0, color = Color.RED, birthMonth = TimePoint(
-            Year(2000), Month.JANUARY)
-        )
-        assertEquals(0, blocks[0].rect.y)
-        assertEquals(240, blocks[0].rect.x)
-        assertEquals(1, blocks[1].rect.y)
-        assertEquals(241, blocks[1].rect.x)
-        assertEquals(Color.RED, blocks[1].color)
-    }
-
-    @Test
-    fun test3InZigZagFormation() {
-        val periods = listOf(
-            Period(TimePoint(Year(2020), Month.JANUARY), TimePoint(Year(2020), Month.MARCH), "Test period 1"),
-            Period(TimePoint(Year(2020), Month.FEBRUARY), TimePoint(Year(2021), Month.MARCH), "Test period 1"),
-            Period(TimePoint(Year(2021), Month.MAY), TimePoint(Year(2025), Month.MAY), "Test period 3")
-        )
-        val blocks = textBlocksForPeriods(
-            periods, baseY = 0, color = Color.BLUE, birthMonth = TimePoint(
-            Year(1979), Month.JULY)
-        )
-        val xs = blocks.map { it.rect.x }
-        val xIsSorted = xs.zipWithNext().all { (a, b) -> a < b }
-        assert(xIsSorted)
-        assertEquals(intArrayOf(0, 1, 0).toList(), blocks.map { it.rect.y })
-    }
-
-    @Test
-    fun homeBlocksAreSortedInX() {
-        val blocks = textBlocksForPeriods(
-            homePeriods(), baseY = 0, color = Color.BLUE, birthMonth = TimePoint(
-            Year(1979), Month.JULY)
-        )
-        val xs = blocks.map { it.rect.x }
-        val xIsSorted = xs.zipWithNext().all { (a, b) -> a < b }
-        assert(xIsSorted)
+    fun test1() {
     }
 }
 
