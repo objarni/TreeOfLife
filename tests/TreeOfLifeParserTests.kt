0012c3b4793cb3f84d7eba8871393ce06734e0fd
Olof Bjarnason <olof.bjarnason@gmail.com>
2025-03-29 11:10:53 +0100

    Add tree of life file format parser

--- tests/TreeOfLifeParserTests.kt
+++ tests/TreeOfLifeParserTests.kt
@@ -1,12 +1,4 @@
-import TreeOfLife.Data.Month
-import TreeOfLife.Data.Period
-import TreeOfLife.Data.TimePoint
-import TreeOfLife.Data.Year
-import TreeOfLife.Data.textBlocksForPeriods
-import TreeOfLife.Data.homePeriods
-import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import java.awt.Color
 
 class TreeOfLifeParserTests {
     @Test
