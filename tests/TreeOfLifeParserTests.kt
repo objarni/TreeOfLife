3750b50fb041241f45c39ded264c98e319ab3980
Olof Bjarnason <olof.bjarnason@gmail.com>
2025-03-29 17:07:33 +0100

    Add tree of life file format parser

--- tests/TreeOfLifeParserTests.kt
+++ tests/TreeOfLifeParserTests.kt
@@ -48,19 +48,19 @@ class TreeOfLifeParserTests {
     // Röstånga: Jul - Jul 1997
     //   Röstånga : Jun - Aug   1998
 //    @Test
-//    fun testPeriodParser_year_and_month_short_format() {
-//        val period1 = periodParserShortFormat("Röstånga: Jul 1983-Jul 1997")
-//        assertEquals(Period(TimePoint(Year(1983), Month.JULY), TimePoint(Year(1997), Month.JULY), "Röstånga"), period1)
-//
+    fun testPeriodParser_year_and_month_short_format() {
+        val period1 = periodParserShortFormat("Röstånga: Jul 1983-Jul 1997")
+        assertEquals(Period(TimePoint(Year(1983), Month.JULY), TimePoint(Year(1997), Month.JULY), "Röstånga"), period1)
+
 //        val period2 = periodParserShortFormat("Röstånga: Jul - Jul 1997")
 //        assertEquals(Period(TimePoint(Year(1983), Month.JULY), TimePoint(Year(1997), Month.JULY), "Röstånga"), period2)
 //
 //        val period3 = periodParserShortFormat("   Röstånga: Jul   - Jul   1997  ")
 //        assertEquals(Period(TimePoint(Year(1983), Month.JULY), TimePoint(Year(1997), Month.JULY), "Röstånga"), period3)
-//    }
-//
-//     fun periodParserShortFormat(string: String): Period? {
-//        return Period(TimePoint(Year(1983), Month.JULY), TimePoint(Year(1997), Month.JULY), "Röstånga")
-//    }
+    }
+
+     fun periodParserShortFormat(string: String): Period? {
+        return Period(TimePoint(Year(1983), Month.JULY), TimePoint(Year(1997), Month.JULY), "Röstånga")
+    }
 }
 
