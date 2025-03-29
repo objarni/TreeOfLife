31af3bc7fa6c85be14eb5c56f4753ea0c2b53f46
Olof Bjarnason <olof.bjarnason@gmail.com>
2025-03-29 13:40:47 +0100

    Add tree of life file format parser

--- tests/TreeOfLifeParserTests.kt
+++ tests/TreeOfLifeParserTests.kt
@@ -1,10 +1,20 @@
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Test
+import kotlin.test.Test
 
 class TreeOfLifeParserTests {
 
     @Test
     fun testSingleLiteralParser() {
+//        var g = object : Grammar<Int>();
+//        val g = object : Grammar<SyntaxTree>() {
+//            val a by literalToken("aa")
+//            val b by literalToken("bb")
+//            override val root = parser { node(lexeme(a) + lexeme(b)) }
+//        }
+//
+//        assertThat(g.parse("bb")).failedWithUnmatchedToken(expected = g.a, offset = 0)
+//        assertThat(g.parse("aa")).failedWithUnmatchedToken(expected = g.b, offset = 2)
+//        assertThat(g.parse("aabbaa")).failedWithUnmatchedToken(expected = EofToken, offset = 4)
         assertEquals(1, 1)
     }
 }
