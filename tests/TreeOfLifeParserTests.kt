ba92b4e28256a96031338adb0dda411cbd6041a1
Olof Bjarnason <olof.bjarnason@gmail.com>
2025-03-29 11:38:33 +0100

    Add tree of life file format parser

--- tests/TreeOfLifeParserTests.kt
+++ tests/TreeOfLifeParserTests.kt
@@ -1,67 +1,13 @@
-import TreeOfLifeParserTests.ExprGrammar.Expr
-import arrow.core.getOrElse
-import io.kpeg.PegParser
-import io.kpeg.pe.Symbol
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import java.awt.Point
+import kotlin.test.expect
 
 class TreeOfLifeParserTests {
 
-    object ExprGrammar {
-
-        val Expr = Symbol.rule<Int>(name = "Expr") {
-            seq {
-                val init = +Num
-                val operations = +Additive.zeroOrMore()
-
-                value {
-                    operations.get.fold(init.get) { lhs, (op, rhs) ->
-                        when (op) {
-                            Op.Add -> lhs + rhs
-                            Op.Sub -> lhs - rhs
-                        }
-                    }
-                }
-            }
-        }
-
-        private val Additive = Symbol.rule<Pair<Op, Int>>(name = "Additive") {
-            seq {
-                val op = +char('+', '-').mapPe { Op.fromChar(it) }
-                val rhs = +Num
-
-                value { op.get to rhs.get }
-            }
-        }
-
-        private val Num = Symbol.rule<Int>(name = "Num", ignoreWS = false) {
-            seq {
-                val sign = +char('+', '-').orDefault('+')
-                val digits = +DIGIT.oneOrMore().joinToString()
-
-                value { (sign.get + digits.get).toInt() }
-            }
-        }
-
-        private enum class Op {
-            Add,
-            Sub;
-
-            companion object {
-                fun fromChar(c: Char) = when (c) {
-                    '+' -> Add
-                    '-' -> Sub
-                    else -> error("")
-                }
-            }
-        }
-    }
-
-    fun evalExpr(expression: String) = PegParser.parse(symbol = Expr.value(), expression).getOrElse { null }
-
     @Test
     fun test1() {
-        val result = evalExpr("1 + 2 - 3")
-        assert(result == 0) { "Expected 0, but got $result" }
+        assertEquals(1, 1)
     }
 }
 
