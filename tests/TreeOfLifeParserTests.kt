5f399fd94bf51d7e7c461db91b3eaf81ea8c4efa
Olof Bjarnason <olof.bjarnason@gmail.com>
2025-03-29 13:45:47 +0100

    Add tree of life file format parser

--- tests/TreeOfLifeParserTests.kt
+++ tests/TreeOfLifeParserTests.kt
@@ -4,17 +4,7 @@ import kotlin.test.Test
 class TreeOfLifeParserTests {
 
     @Test
-    fun testSingleLiteralParser() {
-//        var g = object : Grammar<Int>();
-//        val g = object : Grammar<SyntaxTree>() {
-//            val a by literalToken("aa")
-//            val b by literalToken("bb")
-//            override val root = parser { node(lexeme(a) + lexeme(b)) }
-//        }
-//
-//        assertThat(g.parse("bb")).failedWithUnmatchedToken(expected = g.a, offset = 0)
-//        assertThat(g.parse("aa")).failedWithUnmatchedToken(expected = g.b, offset = 2)
-//        assertThat(g.parse("aabbaa")).failedWithUnmatchedToken(expected = EofToken, offset = 4)
+    fun testMonthParser() {
         assertEquals(1, 1)
     }
 }
