54d1a08ebc63d2aec0260b355c0eb0ef1236ded2
Olof Bjarnason <olof.bjarnason@gmail.com>
2025-03-29 14:17:46 +0100

    Add tree of life file format parser

--- tests/TreeOfLifeParserTests.kt
+++ tests/TreeOfLifeParserTests.kt
@@ -23,7 +23,6 @@ class TreeOfLifeParserTests {
     //   Röstånga : Jun - Aug   1998
     @Test
     fun testPeriodParser_year_and_month_format() {
-
         // Test with a valid period string, canonical format
         val period1 = periodParser("Röstånga: Jul 1983-Jul 1997")
         assertEquals(Period(TimePoint(Year(1983), Month.JULY), TimePoint(Year(1997), Month.JULY), "Röstånga"), period1)
