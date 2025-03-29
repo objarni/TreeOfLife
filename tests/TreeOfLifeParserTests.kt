bc40674b2419bc9ee618b3ce4e299f4fbbb929b3
Olof Bjarnason <olof.bjarnason@gmail.com>
2025-03-29 13:52:37 +0100

    Add tree of life file format parser

--- tests/TreeOfLifeParserTests.kt
+++ tests/TreeOfLifeParserTests.kt
@@ -12,6 +12,13 @@ class TreeOfLifeParserTests {
         assertNull(monthParser("sososo"))
     }
 
+    // Test data for the period parser
+    // Röstånga: Jul 1983-Jul 1997
+    // Röstånga: Jul 1983 - Jul 1997
+    //   Röstånga : Jun - Aug   1998
+    
+
+
     private fun monthParser(string: String): Month? {
         val month = string.lowercase()
         if (month.startsWith("jan")) return Month.JANUARY
