f7f252a4c8c3d1ce7b2401cba613371a657f4d19
Olof Bjarnason <olof.bjarnason@gmail.com>
2025-03-29 13:49:56 +0100

    Add tree of life file format parser

--- tests/TreeOfLifeParserTests.kt
+++ tests/TreeOfLifeParserTests.kt
@@ -1,6 +1,7 @@
 import TreeOfLife.Data.Month
 import org.junit.jupiter.api.Assertions.assertEquals
 import kotlin.test.Test
+import kotlin.test.assertNull
 
 class TreeOfLifeParserTests {
 
@@ -8,13 +9,14 @@ class TreeOfLifeParserTests {
     fun testMonthParser() {
         assertEquals(Month.JANUARY, monthParser("Jan"))
         assertEquals(Month.FEBRUARY, monthParser("Feb"))
+        assertNull(monthParser("sososo"))
     }
 
     private fun monthParser(string: String): Month? {
         val month = string.lowercase()
         if (month.startsWith("jan")) return Month.JANUARY
         if (month.startsWith("feb")) return Month.FEBRUARY
-        return Month.JANUARY
+        return null
     }
 }
 
