eb88d82baaa38c672f654082c38da918001799ef
Olof Bjarnason <olof.bjarnason@gmail.com>
2025-04-05 19:40:19 +0200

    Set origo time point from data in text file

--- tests/TreeOfLifeFileBehaviourTests.kt
+++ tests/TreeOfLifeFileBehaviourTests.kt
@@ -1,12 +1,4 @@
-import TreeOfLife.Data.Month
-import TreeOfLife.Data.Period
-import TreeOfLife.Data.TimePoint
-import TreeOfLife.Data.Year
-import TreeOfLife.Data.textBlocksForPeriods
-import TreeOfLife.Data.homes
-import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import java.awt.Color
 
 interface TreeOfLifeTextFileOperations {
     fun readWholeFile(): String?
