d57584c1cbaa62e995f2265981b320d050edf028
Olof Bjarnason <olof.bjarnason@gmail.com>
2025-05-01 20:32:40 +0200

    Decrease year marker lengths

--- src/Visualization/TimelinePanel.kt
+++ src/Visualization/TimelinePanel.kt
@@ -141,7 +141,7 @@ class TimelinePanel() : JPanel(), MouseWheelListener, KeyListener, MouseListener
         val origo = projector.projectPoint(Point(age * 12, 0))
         val origoText = String.format(template, birthMonth.month.name().substring(0, 3), birthMonth.year.value + age)
         g2d.drawString(origoText, origo.x, origo.y + 3*zoom.toInt())
-        g2d.drawLine(origo.x, origo.y, origo.x, origo.y + 5*zoom.toInt())
+        g2d.drawLine(origo.x, origo.y, origo.x, origo.y + 2*zoom.toInt())
     }
 
     override fun keyTyped(e: KeyEvent?) {}
